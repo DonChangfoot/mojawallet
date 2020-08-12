68ceec854fc5f1dedb5a07664c0aee80b4235cd3
donchangfoot <donovanchangfoot@gmail.com>
2020-08-12 10:52:19 +0200

    fix(frontend): make pusher key configurable

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -8,6 +8,7 @@ module.exports = withCSS({
         REACT_APP_GRANT_URL: process.env.REACT_APP_GRANT_URL || 'hydra-admin:4445/oauth2/auth\?client_id\=frontend-service\&state\=loginflow\&response_type\=code\&redirect_uri\=https://mojaloop.app/callback',
         HYDRA_REDIRECT_URI: process.env.HYDRA_REDIRECT_URI || 'https://mojaloop.app/callback',
         REACT_APP_LOGIN_GRANT_URL: process.env.REACT_APP_LOGIN_GRANT_URL || 'hydra-admin:4445/oauth2/auth?client_id=frontend-service&state=loginflow&response_type=code&redirect_uri=https://mojaloop.app/callback',
-        HYDRA_CLIENT_ID: process.env.HYDRA_CLIENT_ID || 'frontend-service'
+        HYDRA_CLIENT_ID: process.env.HYDRA_CLIENT_ID || 'frontend-service',
+        PUSHER_KEY: process.env.PUSHER_KEY || null
     }
 });
